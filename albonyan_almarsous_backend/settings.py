13a83c56f423dfd84a009c02a6a5a9d1f63811b0
Alhassanalshareff <alhassan.alshareff@gmail.com>
2025-10-27 10:01:03 +0300

    creat a new database

--- albonyan_almarsous_backend/settings.py
+++ albonyan_almarsous_backend/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = 'albonyan_almarsous_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'albonyan_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Toty@123456',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
